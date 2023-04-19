9884ab51da753a58af6305b9630893d1125540cc
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-19 14:49:26 +0900

    feat: BufferdReaderEx, BufferedReaderTest 클래스 생성

---
+++ src/main/java/com/example/javaproject2/week1/BufferedReaderEx.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderEx {
+    public void readALine() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());
+    }
+    public void readTwoLine() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());
+        System.out.println(br.readLine());
+    }
+}
