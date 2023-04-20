e78b7081b829e7f40d37f4e92170ab930ae000a7
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-20 13:49:08 +0900

    feat: codeup 1011-1013 풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Pro1012.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Pro1012 {
+    public static void main(String[] args) {
+        System.out.printf("%.6f",new Scanner(System.in).nextFloat());
+    }
+}
