fa67fdcdc28d656075d6c23e43c2b5a09189df45
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-20 11:29:55 +0900

    feat: MultiplicationTable 클래스 생성

--- src/main/java/com/example/javaproject2/week1/day4/ScannerEx2Test.java
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerEx2Test.java
@@ -3,6 +3,6 @@ package com.example.javaproject2.week1.day4;
 public class ScannerEx2Test {
     public static void main(String[] args) {
         ScannerEx2 sc = new ScannerEx2();
-        sc.inputTwoNumberAndPlus();
+        sc.inputTwoNumbers();
     }
 }
