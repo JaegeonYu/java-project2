96043a6390031124262e43f0436792a067978598
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-20 15:00:44 +0900

    feat: TwoSum 클래스 생성

---
+++ src/main/java/com/example/javaproject2/week1/day4/TwoSum.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1.day4;
+
+import java.util.Scanner;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        System.out.println("두 수의 합은 :" + val1 + val2);
+    }
+}
