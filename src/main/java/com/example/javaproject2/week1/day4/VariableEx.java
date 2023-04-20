e258e7ab93cbf3d3cdf943abf28d10b83dbeae6d
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-20 15:17:15 +0900

    feat: VariableEx 클래스 생성

---
+++ src/main/java/com/example/javaproject2/week1/day4/VariableEx.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week1.day4;
+
+import com.example.javaproject2.PrintHello;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        int iVal;
+        iVal = 0;
+
+        PrintHello printHello;
+        printHello = new PrintHello();
+        printHello.print();
+    }
+}
