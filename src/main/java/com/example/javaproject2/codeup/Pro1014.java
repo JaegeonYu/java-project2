2fa45b678640f7ed9b4249f2191ce328434ce6dc
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-20 15:31:14 +0900

    feat: codeup 1014 풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Pro1014.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Pro1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String first = sc.next();
+        String second = sc.next();
+        System.out.println(second +" " + first);
+    }
+}
