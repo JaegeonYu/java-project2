a432c263701e37c54b7a25fce8106a9dee5c486b
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-21 11:12:16 +0900

    feat: codeup 1018 풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Pro1018.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Pro1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String val = sc.nextLine();
+        System.out.println(val);
+    }
+}
