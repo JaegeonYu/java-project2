d5804076464522d64d8890e16bd9fdfa9019c5a9
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-24 15:06:06 +0900

    feat: FloatPlus 클래스 생성

---
+++ src/main/java/com/example/javaproject2/week2/day6/FloatPlus.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week2.day6;
+
+import java.util.Arrays;
+
+public class FloatPlus {
+    public static void main(String[] args) {
+        String val1 = "1.11111111";
+        String val2 = "2.11111111";
+        float result = Float.parseFloat(val1) + Float.parseFloat(val2);
+
+        System.out.println(result);
+    }
+}
