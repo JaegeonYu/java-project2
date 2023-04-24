ce17b5688d205e42f45fdeb0c5d6d14d53f4bdd7
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-24 13:35:53 +0900

    feat: NarrowingCasting 클래스 생성

---
+++ src/main/java/com/example/javaproject2/week2/day6/NarrowingCasting.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week2.day6;
+
+public class NarrowingCasting {
+    public static void main(String[] args) {
+        double dVal = 123.456;
+        int val = (int)dVal;
+
+        System.out.println("dVal = "+ dVal);
+        System.out.println("val = " + val);
+    }
+}
