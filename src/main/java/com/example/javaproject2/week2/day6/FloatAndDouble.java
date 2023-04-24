80197695488d4b8ccca2e7bd74f57d9c871dfcfb
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-24 13:34:01 +0900

    feat: Float, Double Class 생성

---
+++ src/main/java/com/example/javaproject2/week2/day6/FloatAndDouble.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week2.day6;
+
+public class FloatAndDouble {
+    public static void main(String[] args) {
+        float f1 = 1.23000008f;
+        double d10 = 1.23000008;
+
+        System.out.println(f1);
+        System.out.println(d10);
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week2/day6/FloatDigit.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week2.day6;
+
+public class FloatDigit {
+    public static void main(String[] args) {
+        float f1= 1.23e5f;
+        float f2= 1.23e6f;
+        float f3= 1.23e7f;
+        float f4= 1.23e8f;
+
+        System.out.println("f1 = "+f1);
+        System.out.println("f2 = "+f2);
+        System.out.println("f3 = "+f3);
+        System.out.println("f4 = "+f4);
+    }
+}
