d0b076f5c4749141ed01e16f29e709352224b755
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-27 14:07:11 +0900

    feat: codeup 1050-57 풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Pro1056.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Pro1056 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+
+        if((val1 == 1 || val2 == 0) || (val1 == 0 || val2 == 1)) System.out.println(1);
+        else System.out.println(0);
+    }
+}
