26f5154215405a5849f061fba52a52617b90278c
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-28 10:16:18 +0900

    feat: 제어문, 콜라츠, codeup 1065-1067 풀이

---
+++ src/main/java/com/example/javaproject2/challange/Collatz.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.challange;
+
+public class Collatz {
+    int solution(int num) {
+        int cnt = 0;
+        long n = num;
+        if (num == 1) return 0;
+        while (n != 1 && cnt < 500) {
+            if (n % 2 == 0) n /= 2;
+            else n = n * 3 + 1;
+            cnt++;
+        }
+        if (cnt == 500) return -1;
+        else return cnt;
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/Pro1066.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Pro1066 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[3];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] % 2== 0)
+                System.out.println("even");
+            else
+                System.out.println("odd");
+        }
+    }
+}
