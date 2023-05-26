04115e2f37122d4fca6551887fe6d8e8d7182901
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-05-26 13:40:41 +0900

    Feat: Interface

---
+++ src/main/java/com/example/javaproject2/week6/day5/Main.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week6.day5;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        System.out.println(circle.calculateArea());
+    }
+}
